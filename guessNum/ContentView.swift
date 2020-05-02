9852884c123c5e80034d15f7224d3004d4fddc75
kuojed <kuojed@gmail.com>
2020-05-02 20:19:03 +0800

    Initial Commit

---
+++ guessNum/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  guessNum
+//
+//  Created by 郭冠杰 on 2020/5/2.
+//  Copyright © 2020 郭冠杰. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
